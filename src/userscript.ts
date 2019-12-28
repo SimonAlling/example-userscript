61c385a676d7827e4549257d54b8fe1f5a7c1379
Simon Alling <alling.simon@gmail.com>
2019-12-29 00:11:09 +0100

    Update metadata

--- src/userscript.ts
+++ src/userscript.ts
@@ -1,11 +1,12 @@
 export default {
-    id: "bootstrapped-userscript",
-    name: "Bootstrapped Userscript",
+    id: "example-userscript",
+    name: "Example Userscript",
     version: "0.1.0",
-    description: "A basic bootstrapped userscript.",
+    description: "A basic example userscript.",
     author: "John Smith",
     hostname: "example.com",
     sitename: "Example.com",
     namespace: "mywebsite.example",
     runAt: "document-start",
+    hostedAt: "https://example.com/userscripts/",
 } as const;
